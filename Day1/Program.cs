a6795f2fcfbeb65a12ce6cf63adbec692c7118a1
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2023-07-14 04:25:07 +0300

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+
+string path = Path.Combine(AppContext.BaseDirectory, "input.txt");
+string input = File.ReadAllText(path, Encoding.UTF8);
+
+input.ToCharArray();
+
+var forwardCount = 0;
+var backwardCount = 0;
+var index = 1;
+var count = 0;
+
+foreach (char c in input)
+{
+    if (c == '(')
+    {
+        forwardCount++;
+    }
+    else if (c == ')')
+    {
+        backwardCount++;
+    }
+}
+
+Console.WriteLine(forwardCount - backwardCount);
+
+foreach (char c in input)
+{
+    if (c == '(')
+    {
+        count++;
+    }
+    else if (c == ')')
+    {
+        count--;
+    }
+    if (count == -1) 
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
