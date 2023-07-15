1281e015d3f33541ecf11d9a01d1d0a135b90731
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2023-07-15 17:49:13 +0300

    Final solution

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -8,33 +8,3 @@ string[] input = File.ReadAllLines(path, Encoding.UTF8);
 var result = Helper.Splitter(input, 'x');
 Console.WriteLine(Helper.CalculateSurfaceArea(result));
 Console.WriteLine(Helper.CalculateRibbon(result));
-
-
-//int neededWrapAll = 0;
-//for (int i = 0; i < input.Length; i++)
-//{
-//    string[] stringDimensions = input[i].Split("x");
-//    int[] intDimensions = new int[3];
-//    int volume = 0;
-
-//    for (int j = 0; j < stringDimensions.Length; j++)
-//    {
-//        int dimension = int.Parse(stringDimensions[j]);
-//        intDimensions[j] = dimension;
-//    }
-//    int[] area = new int[3];
-//    area[0] = (2 * intDimensions[0] * intDimensions[1]);
-//    area[1] = (2 * intDimensions[0] * intDimensions[2]);
-//    area[2] = (2 * intDimensions[1] * intDimensions[2]);
-
-//    volume = area[0] + area[1] + area[2];
-
-//    int minimum = area.Min() / 2;
-//    int neededWrapEach = volume + minimum;
-
-//    neededWrapAll += neededWrapEach;
-//}
-
-//Console.WriteLine(neededWrapAll);
-
-
